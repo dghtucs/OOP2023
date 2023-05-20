a921d3553eccd7324b21389178e9b34723344058
dgh <tudghian2020@qq.com>
2023-05-21 01:16:17 +0800

    n

--- C++面向对象/ppp.cpp
+++ C++面向对象/ppp.cpp
@@ -36,7 +36,9 @@ int main()
     Person p1;
     p1.age = 5;
     //p1.id = 9;
+    p1.setId(9);
     //p1.name = "tql";
+    p1.setName("tql");
     cout << p1.getAge() << " " << p1.getId() << " " << p1.getName()<<endl;;
     Hr h;h.age = 1;
     h.setId(2);h.setName("ddd");
