2837f54d64dd30261d5d16343c81d503fb42be9d
dgh <tudghian2020@qq.com>
2023-05-21 00:37:43 +0800

    simple class

--- C++面向对象/myClass.cpp
+++ C++面向对象/myClass.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
+
+class Person
+{
+    public:
+        int age;
+        int id;
+        string name;
+        int getId(){return this->id;}
+        string getName(){return this->name;}
+        int getAge();
+};
+
+int Person::getAge()
+{
+    return this->age;
+}
 
 int main()
 {
-    
+    Person p1,p2,p3;
+    p1.age = 5;
+    p1.id = 9;
+    p1.name = "tql";
+    cout << p1.getAge() << " " << p1.getId() << " " << p1.getName();
 }
