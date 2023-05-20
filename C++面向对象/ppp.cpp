19b11b6ed5b34e1d83a0fb223477d48dbe9a21c4
dgh <tudghian2020@qq.com>
2023-05-21 01:13:44 +0800

    public protected private

---
+++ C++面向对象/ppp.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+class Person
+{
+    
+    private:
+        int id;
+    protected:
+        string name;
+    public:
+        int age;
+        int setId(int i){this->id = i;}
+        void setName(string s){this->name = s;}
+        int getId(){return this->id;}
+        string getName(){return this->name;}
+        int getAge();
+};
+class Hr : public Person
+{
+    public:
+        void printName()
+        {
+            cout << this->name << endl;
+        }
+        //void printId(){cout << this->id << endl;}
+};
+int Person::getAge()
+{
+    return this->age;
+}
+
+int main()
+{
+    Person p1;
+    p1.age = 5;
+    //p1.id = 9;
+    //p1.name = "tql";
+    cout << p1.getAge() << " " << p1.getId() << " " << p1.getName()<<endl;;
+    Hr h;h.age = 1;
+    h.setId(2);h.setName("ddd");
+    h.printName();//h.printId();
+}
