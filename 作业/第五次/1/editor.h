d93098aa8d7ccc4483c01b7bd47ddfa2d461a56a
dgh <tudghian2020@qq.com>
2023-05-23 16:51:48 +0800

    Update editor.h

--- 作业/第五次/1/editor.h
+++ 作业/第五次/1/editor.h
@@ -9,6 +9,8 @@ private:
     using Position = std::pair<int, int>;
     std::vector<std::string> data; // 数据区，亦可采用其他数据结构
     std::vector<std::string> clipboard; // 剪贴板，亦可采用其他数据结构
+    Position p;
+    bool isSelect;
 public:
     TextEditor();
     void moveCursor(Position dest);
@@ -20,3 +22,43 @@ public:
     void paste();
     void screenShot();
 };
+
+TextEditor::TextEditor()
+{
+    this->p = std::make_pair(0,0);
+    this->isSelect = false;
+}
+void TextEditor::moveCursor(Position dest)
+{
+    if(this->isSelect)
+        this->isSelect = false;
+    this->p = dest;
+}
+void TextEditor::select(Position beg, Position end)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
