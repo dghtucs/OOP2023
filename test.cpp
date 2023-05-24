22e53e2b3cb026948020ccf67ccda582a801c609
dgh <tudghian2020@qq.com>
2023-05-24 21:51:48 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,32 +1,13 @@
-// #include <iostream>
-#include <iosfwd>
+#include <iostream>
 
-// 前置声明，无需包含完整的头文件
-class MyClass;
+int main(int argc, char* argv[])
+{
+    std::cout << "Number of command line arguments: " << argc << std::endl;
 
-// 使用前置声明的函数
-void printValue(const MyClass& obj);
-
-// 定义 MyClass 类
-class MyClass {
-public:
-    MyClass(int value) : value_(value) {}
-
-private:
-    int value_;
-
-    // 友元函数，需要访问 MyClass 的私有成员
-    friend void printValue(const MyClass& obj);
-};
-
-// 使用完整的 MyClass 定义的函数
-void printValue(const MyClass& obj) {
-    std::cout << "Value: " << obj.value_ << std::endl;
-}
-
-int main() {
-    MyClass obj(42);
-    printValue(obj);
+    for (int i = 0; i < argc; ++i)
+    {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    }
 
     return 0;
 }
