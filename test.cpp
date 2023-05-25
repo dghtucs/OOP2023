d106c5ac92535cce9061e5d2f7e2659d61f0006b
dgh <tudghian2020@qq.com>
2023-05-25 18:33:55 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+using namespace std;
+constexpr int square(int x) {
+    return x + x;
+}
 
-int main(int argc, char* argv[])
-{
-    std::cout << "Number of command line arguments: " << argc << std::endl;
+int main() {
+    constexpr int num = 5;
+    constexpr int result = square(num); // 编译时计算 square(num)
 
-    for (int i = 0; i < argc; ++i)
-    {
-        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
-    }
+    std::cout << "Result: " << result << std::endl; // 编译时已知结果，输出 "Result: 25"
 
+    int i;
+    cin >> i;
+    int two = 2*i;
+    cout << two << endl;
     return 0;
 }
