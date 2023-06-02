9f34200cf650861efbe0d9cba5ce2379465d8c44
dgh <tudghian2020@qq.com>
2023-06-02 16:22:28 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
 using namespace std;
-constexpr int square(int x) {
-    return x + x;
-}
+
 
 int main() {
-    constexpr int num = 5;
-    constexpr int result = square(num); // 编译时计算 square(num)
-
-    std::cout << "Result: " << result << std::endl; // 编译时已知结果，输出 "Result: 25"
-
-    int i;
-    cin >> i;
-    int two = 2*i;
-    cout << two << endl;
-    //bianyishi
+    int i = 1;
+    i = i++;
+    cout << i << endl;
     return 0;
 }
